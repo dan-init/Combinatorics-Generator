314d380c3a4b8f7324ca6f5269076ad808be82ba
dan-init <101570099+dan-init@users.noreply.github.com>
2022-03-15 14:18:45 +0000

    Update Combinatorics Generator.py
    - Altered layout, more user friendly
    - Added function for error messagebox if check-boxes are left inactive
    
    Tried to add a condition in the display_error function for an error message box to display if str is entered into the Entries.

--- Combinatorics Generator.py
+++ Combinatorics Generator.py
@@ -10,15 +10,22 @@ n = IntVar()
 k = IntVar()
 
 def display_results():
-    """Displays messagebox depending on which checkbox is selected"""
+    """Displays messagebox depending on which checkboxes are selected"""
     if permutations_checkbox_state.get() == 1 and repeat_checkbox_state.get() == 0:
         permutations_no_repeats(n, k)
     elif permutations_checkbox_state.get() == 1 and repeat_checkbox_state.get() == 1:
         permutations_with_repeats(n, k)
-    elif combinations_checkbox_state.get() == 1 and repeat_checkbox_state.get() ==0:
+    elif combinations_checkbox_state.get() == 1 and repeat_checkbox_state.get() == 0:
         combinations_no_repeats(n, k)
     elif combinations_checkbox_state.get() == 1 and repeat_checkbox_state.get() == 1:
         combinations_with_repeats(n,k)
+    else:
+        display_error_message()
+
+def display_error_message():
+    """Displays error message if prerequesite conidtions are not met"""
+    if permutations_checkbox_state.get() == 0 and combinations_checkbox_state.get() == 0 and repeat_checkbox_state.get() == 0 or 1:
+        messagebox.showerror('Error', 'Please select Combinations or Permutations!')
 
 def permutations_no_repeats(n:int, k:int) -> int:
     """Returns number of permutations possible where numbers cannot repeat"""
@@ -35,7 +42,7 @@ def permutations_with_repeats(n:int, k:int) -> int:
     'Permutations with repeat numbers: '+ str("{:,}".format(repeat_permutations)))
 
 def combinations_no_repeats(n:int, k:int) -> int:
-    """Function which returns the number of combinations possible from two given integers"""
+    """Returns number of possible combinations where numbers cannot repeat"""
     non_repeat_combinations = (factorial(n.get())//(factorial(k.get())*factorial(n.get()-k.get())))
     
     return messagebox.showinfo('Combination Results', 
@@ -62,7 +69,6 @@ class Gui:
         master.permutation_label = Label(root, text='Permutations')
         master.combination_label = Label(root, text='Combinations')
         master.repeat_label = Label(root, text='Repeats allowed')
-        master.space_label = Label(root, text=' ')
 
         #Checkbuttons
         master.select_permutations = Checkbutton(root, variable=permutations_checkbox_state, onvalue=1, offvalue=0 )
@@ -74,25 +80,26 @@ class Gui:
         master.k_label = Label(root, text='Enter k:')
 
         #Entries
-        master.n_entry = Entry(root, textvariable=n)
-        master.k_entry = Entry(root, textvariable=k)
+        master.n_entry = Entry(root, textvariable=n, width=10, justify='center')
+        master.k_entry = Entry(root, textvariable=k, width=10, justify='center')
 
         #Button
         master.calc = Button(root, text='Calculate', command=display_results)
 
         #Grid
-        master.n_label.grid(row=0, column=0)
-        master.k_label.grid(row=2, column=0)
-        master.n_entry.grid(row=1, column=0, padx=5, pady=2)
-        master.k_entry.grid(row=3, column=0, padx=5, pady=2)
-        master.separator.grid(row=0, column=1, rowspan=6, sticky='ns')
-        master.permutation_label.grid(row=0, column=2)
-        master.combination_label.grid(row=2, column=2)
-        master.repeat_label.grid(row=4, column=2)
-        master.select_permutations.grid(row=1, column=2)
-        master.select_combinations.grid(row=3, column=2)
-        master.select_repeats.grid(row=5, column= 2)
-        master.calc.grid(row=4, column=0, rowspan=2)
+        master.n_label.grid(row=0, column=2)
+        master.k_label.grid(row=2, column=2)
+        master.n_entry.grid(row=1, column=2, padx=10, pady=2)
+        master.k_entry.grid(row=3, column=2, padx=10, pady=2)
+        master.calc.grid(row=4, column=2, rowspan=2)
+        master.separator.grid(row=0, column=1, rowspan=6, sticky='ns', padx=5, pady=5)
+        master.permutation_label.grid(row=0, column=0)
+        master.combination_label.grid(row=2, column=0)
+        master.repeat_label.grid(row=4, column=0)
+        master.select_permutations.grid(row=1, column=0)
+        master.select_combinations.grid(row=3, column=0)
+        master.select_repeats.grid(row=5, column=0)
+
 
 def main():
     Gui(root)
